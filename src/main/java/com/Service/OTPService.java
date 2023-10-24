92bfe65c4e2df68d9c9d3600ed3f9e35fd62a9c2
Kanza Jamal <skanzajamal@gmail.com>
2023-10-24 11:12:25 +0200

    update msg

--- src/main/java/com/Service/OTPService.java
+++ src/main/java/com/Service/OTPService.java
@@ -15,7 +15,7 @@ public class OTPService {
     //method to send OTP
     public void send(RequestDto sms, String trialNumber) {
         var otp = generateOTP();
-        String otpMessage = "Dear Customer, Your OTP is " + otp + ". Please contact our service, if you have not requested for this. Do not share your OTP to anyone. Thank you!";
+        String otpMessage = "Dear Customer, Your OTP is " + otp + ". Please contact our service if you have not requested for this. Do not share your OTP to anyone. Thank you!";
         Message message = Message.creator(new PhoneNumber(sms.getPhoneNumber()),
                 new PhoneNumber(trialNumber),
                 otpMessage)
